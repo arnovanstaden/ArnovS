c5b3af0f44484dacbe2629c588a24f1a04cca665
Arno van Staden <arnovanstaden@gmail.com>
2021-02-04 08:12:54 +0200

    convert to ts add webdacity assets add initial pages

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../styles/index.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+        <h1>Main</h1>
+      </main>
+    </div>
+  )
+}
