167b7ecd241fe49f8e807b82c001285ef1b34746
arnovanstaden <arnovanstaden@gmail.com>
2023-10-21 17:14:27 +0200

    update scroll hook

--- src/hooks/ui.ts
+++ src/hooks/ui.ts
@@ -1,20 +1,12 @@
-import { useEffect, useState } from 'react';
-
 const useScrollToElement = (): {
   scrollToElement: (id: string) => void
 } => {
-  const [id, setId] = useState('');
-
-  useEffect(() => {
-    if (!id) return;
+  const scrollToElement = (id: string) => {
     const targetElement = document.getElementById(id);
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: 'smooth' });
-      setId('');
     }
-  }, [id]);
-
-  const scrollToElement = (elId: string) => setId(elId);
+  };
 
   return { scrollToElement };
 };
